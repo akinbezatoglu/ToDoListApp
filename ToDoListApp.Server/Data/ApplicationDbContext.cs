baa324bf6ddbea1f68f45d675795b3dea77304f2
akinbezatoglu <akinbezatoglu@gmail.com>
2024-08-17 17:43:41 +0300

    Update user properties configuration

--- ToDoListApp.Server/Data/ApplicationDbContext.cs
+++ ToDoListApp.Server/Data/ApplicationDbContext.cs
@@ -24,15 +24,23 @@ namespace ToDoListApp.Server.Data
         {
             var builder = modelBuilder.Entity<User>();
 
+            builder.HasIndex(x => x.ReferenceId)
+                .IsUnique();
+
             builder.HasIndex(x => x.Email)
                 .IsUnique();
 
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(250);
+
             builder.Property(x => x.Fullname)
                 .IsRequired()
-                .HasMaxLength(200);
+                .HasMaxLength(250);
 
-            builder.HasIndex(x => x.ReferenceId)
-                .IsUnique();
+            builder.Property(x => x.Password)
+                .IsRequired()
+                .HasMaxLength(128);
 
             builder.HasMany(x => x.Todolists)
                 .WithOne(x => x.User)
@@ -49,7 +57,7 @@ namespace ToDoListApp.Server.Data
 
             builder.Property(x => x.Title)
                 .IsRequired()
-                .HasMaxLength(200);
+                .HasMaxLength(250);
 
             builder.Property(x => x.Content)
                 .HasMaxLength(750);
